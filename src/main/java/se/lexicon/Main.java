0d2a1962776f124c45662dd243768dd1e983f472
ornella28 <angelnibaruta88@gmail.com>
2026-01-13 10:13:40 +0100

    Creating a list of persons

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,6 +1,22 @@
 package se.lexicon;
 
 
+import java.util.List;
+
 public class Main {
+
+    void main(){
+        List<Person> people = List.of(
+                new Person("Amina", 22, "Stockholm", true),
+                new Person("Erik", 17, "Uppsala", true),
+                new Person("Noah", 34, "Stockholm", false),
+                new Person("Sara", 29, "Göteborg", true),
+                new Person("Lina", 41, "Malmö", false),
+                new Person("Omar", 19, "Stockholm", true)
+        );
+
+    }
+
+
 
 }
