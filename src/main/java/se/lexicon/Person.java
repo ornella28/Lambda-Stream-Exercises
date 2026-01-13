fda6f7eb7481c9c7aa7057454835c04059566b0e
ornella28 <angelnibaruta88@gmail.com>
2026-01-13 10:06:59 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/Person.java
@@ -0,0 +1,54 @@
+package se.lexicon;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private String city;
+    private boolean active;
+
+    // Constructor
+    public Person(String name, int age, String city, boolean active) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.active = active;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                ", active=" + active +
+                '}';
+    }
+}
+
